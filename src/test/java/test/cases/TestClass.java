a9e8e03233748da6293319a778618170a0a29081
ritiz <rjaat@yprime.com>
2023-07-30 16:04:13 +0530

    first commit

---
+++ src/test/java/test/cases/TestClass.java
@@ -0,0 +1,16 @@
+package test.cases;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestClass {
+
+	@Test
+	public void passTestCase() {
+		Assert.assertEquals(true, true);
+	}
+	
+	@Test
+	public void failTestCase() {
+		Assert.assertEquals(false,true);
+	}
+}
